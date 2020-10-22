b70075a50ca8f24f067835de7e0d9dd8072fbe04
Adam Pan <adampan94@gmail.com>
2020-10-21 22:20:27 -0700

    Modify prod build to fix deployment

--- db.js
+++ db.js
@@ -6,13 +6,18 @@ const devConfig = {
   password: process.env.PG_PASSWORD,
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE
+  database: process.env.PG_DATABASE,
 };
 
 const prodConfig = {
-  connectionString: process.env.DATABASE_URL // heroku addons 
-}
+  connectionString: process.env.DATABASE_URL, // heroku addons
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
-const pool = new Pool(process.env.NODE_ENV === 'production' ? prodConfig : devConfig);
+const pool = new Pool(
+  process.env.NODE_ENV === 'production' ? prodConfig : devConfig
+);
 
 module.exports = pool;
